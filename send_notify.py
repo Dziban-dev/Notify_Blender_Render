cc6e459abf0cd264e261e288ee84dc7efa7c721f
Dziban.dev <36001673+Dziban-dev@users.noreply.github.com>
2018-11-18 00:15:57 -0600

    Update send_notify.py

--- send_notify.py
+++ send_notify.py
@@ -13,7 +13,7 @@
 bl_info = {
     "name": "Notify",
     "author": "Dziban",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (2, 79, ),
     "location": "Global",
     "description": "Sends a system notification when render is complete.",
@@ -21,14 +21,43 @@ bl_info = {
     "wiki_url": "",
     "category": "System",
     }
-import bpy, os, subprocess
+import bpy, os, locale, subprocess
 from bpy.app.handlers import persistent
+loc = locale.getlocale() # get current locale
+locx = loc[:3]
+locale.getdefaultlocale()
 @persistent
 def is_render_complete(scene):
-        if os.name=='posix':
+#LINUX
+        if os.name=='posix' and locx=="es_":#Español
+            bashCommand = 'notify-send -u "critical" -i "blender" "Blender | Render Finalizado!"'
+        elif os.name=='posix' and locx=="ca_":#Catalán
+            bashCommand = 'notify-send -u "critical" -i "blender" "Blender | S´ha finalitzat la prestació!"'
+        elif os.name=='posix' and locx=="fr_":#Frances
+            bashCommand = 'notify-send -u "critical" -i "blender" "Blender | Rendu terminé!"'
+        elif os.name=='posix' and locx=="it_":#Italiano
+            bashCommand = 'notify-send -u "critical" -i "blender" "Blender | Rendering finito!"'
+        elif os.name=='posix' and locx=="pt_":#Protugues
+            bashCommand = 'notify-send -u "critical" -i "blender" "Blender | Renderizado concluído!"'
+        elif os.name=='posix' and locx=="de_":#Alemán
+            bashCommand = 'notify-send -u "critical" -i "blender" "Blender | Fertig machen!"'
+        else:
             bashCommand = 'notify-send -u "critical" -i "blender" "Blender | Render Finished!"'
         output = subprocess.check_output(['bash','-c', bashCommand])
-        if os.name=='nt':
+#WINDOWS
+        if os.name=='nt' and locx=="es_":#Español
+            command = 'msg * /server:%computername% "blender" "Blender | Render Finalizado!"'
+        elif os.name=='nt' and locx=="ca_":#Catalán
+            command = 'msg * /server:%computername% "blender" "Blender | S´ha finalitzat la prestació!"'
+        elif os.name=='nt' and locx=="fr_":#Frances
+            command = 'msg * /server:%computername% "blender" "Blender | Rendu terminé!"'
+        elif os.name=='nt' and locx=="it_":#Italiano
+            command = 'msg * /server:%computername% "blender" "Blender | Rendering finito!"'
+        elif os.name=='nt' and locx=="pt_":#Protugues
+            command = 'msg * /server:%computername% "blender" "Blender | Renderizado concluído!"'
+        elif os.name=='nt' and locx=="de_":#Alemán
+            command = 'msg * /server:%computername% "blender" "Blender | Fertig machen!"'
+        else:
             command = 'msg * /server:%computername% "blender" "Blender | Render Finished!"'
 def register():
      bpy.app.handlers.render_complete.append(is_render_complete)
